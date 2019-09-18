7d58fcf22812d31965c7ce2260449593b9ecf2c8
HG <hg@hgs-macbook-air.lan>
2019-09-18 10:01:53 -0700

    trying to get access to participants database

--- js/main.js
+++ js/main.js
@@ -58,7 +58,11 @@ var tipApp = angular.module('tipApp', ['ngRoute', 'ui.router']);
     });
     
     tipApp.controller('startController', function($scope, $rootScope) {
+        $scope.message="hello"
+        var db = firebase.firestore(); 
+        $scope.participants = db.collection("participants").get(); 
     });
+    console.log($scope.participants); 
 
     tipApp.controller('ambiguousController', function($scope) {
 
